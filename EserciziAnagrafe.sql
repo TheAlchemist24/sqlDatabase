5d2f5b16abc8cd81ae697b2d8af847444eeba5a9
TheAlchemist24 <matteo.fraschetti24@gmail.com>
2022-05-02 15:45:29 +0200

    esercizio3

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -10,3 +10,7 @@ select f.nome as figlio, g.Nome as genitore
 from persona f join genia on f.Nome = genia.Figlio join persona g on g.Nome = genia.Genitore
 where f.cittàNascita = g.cittàNascita
 
+/* 3. città in cui è nato almeno un genitore di oltre 50 anni */
+select p.CittàNascita
+from genia g left join persona p on g.Genitore = p.Nome
+where p.Età > 50
