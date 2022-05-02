2ac5d12131027d3938aa4a30c9d0741bdeefd5f1
TheAlchemist24 <matteo.fraschetti24@gmail.com>
2022-05-02 15:39:05 +0200

    esercizio2

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -3,3 +3,10 @@
 select p.nome, c.regione
 from persona p left join città c on p.cittàNascita = c.nome
 where p.età >= 18
+
+/* 2. coppie <genitore,figlio> nati nella stessa città */
+
+select f.nome as figlio, g.Nome as genitore
+from persona f join genia on f.Nome = genia.Figlio join persona g on g.Nome = genia.Genitore
+where f.cittàNascita = g.cittàNascita
+
